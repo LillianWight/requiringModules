a42b2ace7af6e2dc9d302c60a67caa8cebd11ae1
LillianWight <lillian.wight@gmail.com>
2022-03-14 15:19:29 -0600

    End of Lesson 39 Requiring Modules in Practice

---
+++ modules2.js
@@ -0,0 +1,3 @@
+const C = require("./test-module-2");
+const calc2 = new C();
+console.log(calc2.add(2, 5));
---
+++ modules3.js
@@ -0,0 +1,3 @@
+const calc3 = require("./test-module-3");
+console.log(calc3.add(2, 5));
+console.log(calc3.multiply(2, 5));
